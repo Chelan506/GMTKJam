4ac1866af9584c8eab7b2322a2153bbce0cf8968
Colin Tipton <coltipto@ttu.edu>
2023-07-08 13:48:30 -0700

    Fixed reverse steering
    (+ hit the civi pathfinding)

--- GMTKJam/car.gd
+++ GMTKJam/car.gd
@@ -10,18 +10,19 @@ func _ready():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	# Compute movement
-	if Input.is_action_pressed("accelerate"):
-		apply_central_force(engine.rotated(rotation))
-	elif Input.is_action_pressed("decelerate"):
-		apply_central_force(-engine.rotated(rotation))
-	
 	# Rotation
 	rotation_dir = 0
 	if Input.is_action_pressed("left"):
 		rotation_dir = -1
 	elif Input.is_action_pressed("right"):
 		rotation_dir = 1
+	
+	# Compute movement
+	if Input.is_action_pressed("accelerate"):
+		apply_central_force(engine.rotated(rotation))
+	elif Input.is_action_pressed("decelerate"):
+		apply_central_force(-engine.rotated(rotation))
+		rotation_dir *= -1
 		
 	# I hate this so much
 	if linear_velocity.x < -20 || linear_velocity.x > 20 || linear_velocity.y < -20 || linear_velocity.y > 20:
